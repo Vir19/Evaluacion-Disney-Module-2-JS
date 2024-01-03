a97f5d97e90784a6f7193f101cdb74324a0a71e3
Vir19 <viir19@hotmail.com>
2024-01-03 20:04:43 +0100

    create structure-add some styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 'use strict';
-
 console.log('>> Ready :)');
+
+// QUERY SELECTOR
+const inputSearch = document.querySelector('.js__inputSearch');
+const btnSearch = document.querySelector('.js__btnSearch');
