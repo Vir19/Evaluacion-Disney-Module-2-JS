0b96cdf9f37329784f6243d19c610dc9e4f31f64
Vir19 <viir19@hotmail.com>
2024-01-10 18:19:43 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,17 +1,17 @@
-const u=document.querySelector(".js_characters"),s=document.querySelector(".js__favourites"),_=document.querySelector(".js__searchForm"),p=document.querySelector(".js__inputSearch");document.querySelector(".js__btnSearch");let c=[],t=[];document.querySelector(".js_trashCan");function g(e){u.innerHTML+=`
+const h=document.querySelector(".js_characters"),s=document.querySelector(".js__favourites"),_=document.querySelector(".js__searchForm"),g=document.querySelector(".js__inputSearch");document.querySelector(".js__btnSearch");let n=[],a=[];document.querySelector(".js_trashCan");const p=document.querySelector(".js__deleteAllBtn");function v(e){h.innerHTML+=`
     <ul class="character-card" data-id="${e._id}">
       <li class="name li" data-id="${e._id}">${e.name}</li>
       <li class="li" data-id="${e._id}"><img class="pic"
          src=${e.imageUrl}
          alt="${e.name}"></li>
     </ul>
-  `}function h(){for(let e=0;e<c.length;e++)g(c[e]);S()}function f(e){s.innerHTML="",t.forEach(a=>{s.innerHTML+=`
-<ul class="character-card" data-id="${a._id}">
-    <li class="name li" data-id="${a._id}">${a.name}</li>
+  `}function f(){for(let e=0;e<n.length;e++)v(n[e]);S()}function m(e){s.innerHTML="",a.forEach(t=>{s.innerHTML+=`
+<ul class="character-card" data-id="${t._id}">
+    <li class="name li" data-id="${t._id}">${t.name}</li>
     <span class="trash-can js_trashCan">X</span>
-  <li class="li" data-id="${a._id}">
-    <img class="pic" src=${a.imageUrl} alt="${a.name}">
+  <li class="li" data-id="${t._id}">
+    <img class="pic" src=${t.imageUrl} alt="${t.name}">
   </li>
 </ul>
-`})}function m(){s.innerHTML="";for(let e=0;e<t.length;e++)f(t[e])}function S(){document.querySelectorAll(".name").forEach(a=>{a.addEventListener("click",d=>{const n=d.currentTarget,r=n.dataset.id,i=c.find(l=>l._id===parseInt(r)),o=t.findIndex(l=>l._id===parseInt(r));o===-1?t.push(i):t.splice(o,1),localStorage.setItem("favouritesData",JSON.stringify(t)),f(),n.classList.toggle("favourited")})})}function v(){s.addEventListener("click",e=>{const a=e.target;if(a.classList.contains("js_trashCan")){const n=a.closest(".character-card").dataset.id,r=t.findIndex(i=>i._id===parseInt(n));r!==-1&&(t.splice(r,1),localStorage.setItem("favouritesData",JSON.stringify(t)),m())}})}_.addEventListener("submit",e=>{e.preventDefault(),u.innerHTML="",fetch(`//api.disneyapi.dev/character?name=${p.value}`).then(a=>a.json()).then(a=>{c=a.data,h()})});fetch("//api.disneyapi.dev/character?pageSize=50").then(e=>e.json()).then(e=>{console.log(e.data),c=e.data,h();const a=localStorage.getItem("favouritesData");a?t=JSON.parse(a):t=[],m(),v()});
+`})}function d(){s.innerHTML="";for(let e=0;e<a.length;e++)m(a[e])}function S(){document.querySelectorAll(".name").forEach(t=>{t.addEventListener("click",o=>{const c=o.currentTarget,r=c.dataset.id,i=n.find(l=>l._id===parseInt(r)),u=a.findIndex(l=>l._id===parseInt(r));u===-1?a.push(i):a.splice(u,1),localStorage.setItem("favouritesData",JSON.stringify(a)),m(),c.classList.toggle("favourited")})})}function I(){s.addEventListener("click",e=>{const t=e.target;if(t.classList.contains("js_trashCan")){const c=t.closest(".character-card").dataset.id,r=a.findIndex(i=>i._id===parseInt(c));r!==-1&&(a.splice(r,1),localStorage.setItem("favouritesData",JSON.stringify(a)),d())}})}function L(){p.addEventListener("click",e=>{e.preventDefault(),a=[],localStorage.setItem("favouritesData",JSON.stringify(a)),d()})}_.addEventListener("submit",e=>{e.preventDefault(),h.innerHTML="",fetch(`//api.disneyapi.dev/character?name=${g.value}`).then(t=>t.json()).then(t=>{n=t.data,f()})});fetch("//api.disneyapi.dev/character?pageSize=50").then(e=>e.json()).then(e=>{console.log(e.data),n=e.data,f();const t=localStorage.getItem("favouritesData");t?a=JSON.parse(t):a=[],d(),I(),L()});
 //# sourceMappingURL=main.js.map
